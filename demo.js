be971b7877106a34c57a7bb0a99feef237070286
ravindra2002 <113086633+ravindra2002@users.noreply.github.com>
2022-09-08 11:29:07 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,12 @@
+console.log("welcome to node js");
+var a =10;
+var b =20;
+var c = a+b;
+console.log(`a value is ${a} and b value is ${b}
+sum is ${a+b}`);
+if(c==30)
+{
+    console.log("sum is 30")
+}else{
+    console.log("else");
+}
